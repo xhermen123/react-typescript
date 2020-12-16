b44a2d2cf77d46cdefbfb95ab2cd50638e0c18e0
Xhermen <xhermen.nexhimi.dev@gmail.com>
2020-12-16 09:43:19 +0800

    animation test

---
+++ src/components/Animation.tsx
@@ -0,0 +1,144 @@
+/* global window */
+import React, { useState } from "react";
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+
+const letterStyle = {
+  width: "calc(100% - 1rem)",
+  height: "calc(100% - 1rem)",
+  top: "0.5rem",
+  left: "0.5rem",
+  backgroundColor: "#f8efd5",
+  overflow: "hidden",
+  padding: "1rem",
+  boxSizing: "border-box",
+  position: "absolute",
+};
+
+function Letter() {
+  const { scrollYProgress } = useViewportScroll();
+  const scaleAnim = useTransform(scrollYProgress, [0, 0.5, 1], [1, 1, 1.5]);
+  const yPosAnim = useTransform(scrollYProgress, [0, 0.4, 1], [0, -250, -100]);
+
+  return (
+    <motion.div
+      style={{
+        width: "calc(100% - 1rem)",
+        height: "calc(100% - 1rem)",
+        top: "0.5rem",
+        left: "0.5rem",
+        backgroundColor: "#f8efd5",
+        overflow: "hidden",
+        padding: "1rem",
+        boxSizing: "border-box",
+        position: "absolute",
+        // ...letterStyle,
+        scale: scaleAnim,
+        y: yPosAnim,
+      }}
+    >
+      <p style={{ marginTop: 0 }}>Hello, it's been a while!</p>
+      <p>
+        Let's take a look at how we made this animation, we'll discuss the
+        following:
+      </p>
+      <ul>
+        <li>
+          How to <code>useViewportScroll</code> works
+        </li>
+        <li>
+          Why <code>useTransform</code> is useful
+        </li>
+      </ul>
+      <p>
+        Read the article,{" "}
+        <a href="https://blog.sethcorker.com/scroll-animation-with-framer-motion">
+          how to use <code>useViewportScroll</code> and{" "}
+          <code>useTransform</code> in Framer Motion
+        </a>
+      </p>
+    </motion.div>
+  );
+}
+
+const envelopeStyle = {
+  width: "28rem",
+  height: "15rem",
+  scale: 1,
+  position: "fixed",
+  top: "10rem",
+  left: "calc(50% - 14rem)",
+  boxShadow: `rgba(0, 0, 0, 0.5) 0px 0px 150px 10px`,
+};
+
+const frontfaceStyle = {
+  width: "100%",
+  height: "100%",
+  backgroundColor: "#debda1",
+  position: "absolute",
+  left: 0,
+  top: 0,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+function Envelope({ children }: any) {
+  const [ffLayer, setFfLayer] = useState(0);
+  const { scrollYProgress } = useViewportScroll();
+  const scaleAnim = useTransform(scrollYProgress, [0, 0.5, 1], [1, 1.2, 0.8]);
+  const yPosAnim = useTransform(scrollYProgress, [0, 0.5, 1], [0, 100, 200]);
+  const zRotAnim = useTransform(scrollYProgress, [0, 0.5, 1], [0, 3, 0]);
+
+  scrollYProgress.onChange((x) => {
+    setFfLayer(x > 0.4 ? -1 : 0);
+  });
+
+  return (
+    <motion.div
+      style={{
+        // ...envelopeStyle,
+        width: "28rem",
+        height: "15rem",
+        // scale: 1,
+        position: "fixed",
+        top: "10rem",
+        left: "calc(50% - 14rem)",
+        boxShadow: `rgba(0, 0, 0, 0.5) 0px 0px 150px 10px`,
+        scale: scaleAnim,
+        y: yPosAnim,
+        rotateZ: zRotAnim,
+      }}
+    >
+      {children}
+      <div style={{ 
+        // ...frontfaceStyle, 
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#debda1",
+        position: "absolute",
+        left: 0,
+        top: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: ffLayer 
+      }}>
+        <button onClick={() => window.scrollTo(0, 1500)}>Open Me</button>
+      </div>
+    </motion.div>
+  );
+}
+
+const letterSceneStyle = {
+  height: "200vh",
+};
+
+export default function LetterScene() {
+  return (
+    <div style={letterSceneStyle}>
+      <Envelope>
+        <Letter />
+      </Envelope>
+    </div>
+  );
+}
